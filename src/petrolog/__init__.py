8f7bd8b618a00f669329a470cca0d1d09d587d3b
aldanstar <aldanstar@gmail.com>
2025-05-30 09:43:26 +0300

    su

--- src/petrolog/__init__.py
+++ src/petrolog/__init__.py
@@ -1,7 +1,51 @@
+# from import_ipynb import import_file
+# from . import petrolog_note
+#
+# # Импорт необходимых функций из блокнота
+# from .petrolog_note import (
+#     class_dict_to_array,
+#     get_class,
+#     unprotect_files,
+#     files_search,
+#     files_scan,
+#     _unit_by_str,
+#     _BettersizerST_parser,
+#     _BeckmanCoulterLS13320XR_parser,
+#     read,
+#     get_ranges,
+#     overlap,
+#     distribution_by_ranges,
+#     folder_processing
+# )
+#
+# # Инициализация единиц измерения
+# import pint
+# import pint_pandas
+#
+# ureg = pint.UnitRegistry()
+# ureg.Unit.default_format = "~P"
+# pint_pandas.PintType.ureg = ureg
+# pint_pandas.PintType.ureg.default_format = "~P"
+#
+# # Опционально: если хотите сделать функции доступными напрямую
+# __all__ = [
+#     'class_dict_to_array',
+#     'get_class',
+#     'unprotect_files',
+#     'files_search',
+#     'files_scan',
+#     'read',
+#     'get_ranges',
+#     'overlap',
+#     'distribution_by_ranges',
+#     'folder_processing',
+#     'ureg'
+# ]
+
 from import_ipynb import import_file
 from . import petrolog_note
 
-# Импорт необходимых функций из блокнота
+# Импорт только функций из блокнота
 from .petrolog_note import (
     class_dict_to_array,
     get_class,
@@ -18,16 +62,10 @@ from .petrolog_note import (
     folder_processing
 )
 
-# Инициализация единиц измерения
-import pint
-import pint_pandas
+# Импорт ureg из самого блокнота
+from .petrolog_note import ureg
 
-ureg = pint.UnitRegistry()
-ureg.Unit.default_format = "~P"
-pint_pandas.PintType.ureg = ureg
-pint_pandas.PintType.ureg.default_format = "~P"
-
-# Опционально: если хотите сделать функции доступными напрямую
+# Опционально: экспорт функций для прямого доступа
 __all__ = [
     'class_dict_to_array',
     'get_class',
